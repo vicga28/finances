c27f1b24de5cd808060f11eb24999ab5fed671fa
vicga28 <97985206+vicga28@users.noreply.github.com>
2026-02-04 14:03:56 +0100

    Update visavis.py

--- visavis.py
+++ visavis.py
@@ -23,7 +23,7 @@ credentials = {
     "type": "service_account",
     "project_id": st.secrets['project_id'],
     "private_key_id": st.secrets['private_key_id'],
-    "private_key": st.secrets['private_key'].replace('\\\\n', '\\n'),
+    "private_key": st.secrets['private_key'].replace('\\n','\n'),
     "client_email": st.secrets['client_email'],
     "client_id": st.secrets['client_id'],
     "auth_uri": "https://accounts.google.com/o/oauth2/auth",
